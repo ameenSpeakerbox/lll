9d50008695139035b0c04fc01abcdb34c1fd4a2f
Asifk23 <23asifk@gmail.com>
2022-06-06 23:40:13 +0530

    ui updates in third section

--- src/Components/ImageComponent/ImageComponent.js
+++ src/Components/ImageComponent/ImageComponent.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import Rectangle from '../../Assets/images/Rectangle 12.png'
-function ImageComponent ({props}) {
-    console.log(props,props.id)
-    return(
-        
-          <div>
-              <div className="card" >
-                  <div  className='border-b-black-800'>
-                  <img src={props.img} class="card-img-top" alt="..."/>
-                  </div>
-                
-                    <div className="card-body">
-                        <p className='text-primary font-bold  card-text'>{props.text}</p>
-                       
-                    </div>
+import Vector from '../../Assets/images/Group 1.svg'
+
+function ImageComponent({ props }) {
+    console.log(props, props.id)
+    return (
+
+        <div className='relative'>
+
+            <div className='border-b-8 border-blue-800'>
+                <img src={props.img} className="w-full h-72 object-center object-cover	" alt="..." />
+            </div>
+
+            <div className="p-4 h-40" style={{ background: '#F6F6F6' }}>
+                <p className='text-primary font-bold  card-text'>{props.text}</p>
+
+                <div >
+                    <img src={Vector} className="absolute right-0 bottom-0" alt="" />
                 </div>
-              
-          </div>
-        
+            </div>
+        </div>
+
 
     )
-    
+
 }
 export default ImageComponent
--- src/pages/Home/ThirdSection.js
+++ src/pages/Home/ThirdSection.js
@@ -1,46 +1,46 @@
 import React from 'react'
-import { For } from 'react-loops'
 import Rectangle from '../../Assets/images/Rectangle 12.png'
 import Rectangle1 from '../../Assets/images/Rectangle 13.png'
 import Rectangle2 from '../../Assets/images/Rectangle 14.png'
 import ImageComponent from '../../Components/ImageComponent/ImageComponent'
 
-let array_of_object=[
+let array_of_object = [
     {
-id:1,
-img:Rectangle,
-text:"Our proven track-record to “deliver” no matter how big or small your shipment."
+        id: 1,
+        img: Rectangle,
+        text: "Our proven track-record to “deliver” no matter how big or small your shipment."
     },
     {
-id:2,
-img:Rectangle1,
-text:"EFS’s global network of dedicated professionals which results in a truly comprehensive service."
+        id: 2,
+        img: Rectangle1,
+        text: "EFS’s global network of dedicated professionals which results in a truly comprehensive service."
     },
     {
-id:3,
-img:Rectangle2,
-text:"Encouragement of open communication with clients so as to fully understand individual requirements."
+        id: 3,
+        img: Rectangle2,
+        text: "Encouragement of open communication with clients so as to fully understand individual requirements."
     }
 ]
 
-function ThirdSection(){
-    return(
-        <main className='container' >
-                <h1 className='text-primary font-bold text-4xl text-center'>What makes EFS standout ?</h1>
-                
-                <div class="grid grid-cols-3 gap-4">
-                    {array_of_object.map((item)=>{
-                        return(
-                            <div className='content-center' ><ImageComponent props={item}/></div>
-                        )
-                            
-                    })}
-                   
-                    {/* <div className='content-center' ><ImageComponent/></div>
+function ThirdSection() {
+    return (
+        <main className='container py-20 xl:px-20' >
+            <h1 className='text-primary font-medium text-4xl text-center'>What makes EFS stand out?</h1>
+
+            <div class="grid grid-cols-3 gap-8 mt-5">
+                {array_of_object.map((item) => (
+                    <div className='justify-center' >
+                        <ImageComponent props={item} />
+                    </div>
+                )
+
+                )}
+
+                {/* <div className='content-center' ><ImageComponent/></div>
                     <div className='content-center' ><ImageComponent/></div> */}
             </div>
-     
-               
+
+
 
         </main>
     )
