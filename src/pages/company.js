933c9cd361a2bd0877bc6571e4adaec5d42716b2
saran <saranya12808@gmail.com>
2022-07-01 08:52:07 +0530

    second sec

--- src/pages/company.js
+++ src/pages/company.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../Components/Layout/Layout'
 import FirstSection from './Company/FirstSection'
+import SeconSection from './Company/SeconSection'
 
 const company = () => {
   return (
     <Layout>
       <FirstSection />
+      <SeconSection/>
     </Layout>
   )
 }
