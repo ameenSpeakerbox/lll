9815b5b48fb7faff14e50710e2cdbec190d643e1
saran <saranya12808@gmail.com>
2022-07-03 21:49:57 +0530

    font weight

--- src/pages/Company/SeconSection.js
+++ src/pages/Company/SeconSection.js
@@ -6,24 +6,24 @@ return(
             <div class="grid grid-cols-3 gap-16">
 
                     <div>
-                        <div  className='text-white font-normal text-7xl mt-14'>2500+</div>
-                        <div><span className='text-white font-normal text-base '>Projects Executed</span></div>
+                        <div  className='text-white font-bold text-7xl mt-14'>2500+</div>
+                        <div><span className='text-white font-bold text-base '>Projects Executed</span></div>
                     </div>
                     
                     <div class="grid grid-rows-2 gap-12 ">
                         <div>
-                        <div className=' text-white font-normal text-7xl ' >150+</div>
-                        <div><span className='text-white font-normal text-base'>Team Members</span></div>
+                        <div className=' text-white font-bold text-7xl ' >150+</div>
+                        <div><span className='text-white font-bold text-base'>Team Members</span></div>
                         </div>
                         <div>
-                        <div className=' text-white font-normal text-7xl ' >25+</div>   
-                        <div><span className='text-white font-normal text-base'>Years of Experience</span></div> 
+                        <div className=' text-white font-bold text-7xl ' >25+</div>   
+                        <div><span className='text-white font-bold text-base'>Years of Experience</span></div> 
                         </div>
                         
                     </div>
                     <div>
-                    <div className=' text-white font-normal text-7xl mt-14'>1700+</div>
-                    <div><span className='text-white font-normal text-base '>Happy Clients</span></div> 
+                    <div className=' text-white font-bold text-7xl mt-14'>1700+</div>
+                    <div><span className='text-white font-bold text-base '>Happy Clients</span></div> 
                     </div>
                     
             </div>
