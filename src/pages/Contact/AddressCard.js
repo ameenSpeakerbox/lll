70731a7d9c25c480033f9704ba807687651d960b
Asifk23 <23asifk@gmail.com>
2022-12-02 09:29:17 +0530

    minor bug fix on build

--- src/pages/Contact/AddressCard.js
+++ src/pages/Contact/AddressCard.js
@@ -5,7 +5,7 @@ const AddressCard = ({ data }) => {
         <div className='text-primary'>
             <h3 className='text-secondary text-xl underline underline-offset-4	'>{data?.location}</h3>
             <p className='mb-5 w-56 mt-3'>{data?.addres}</p>
-            <a href={data.mapAddress} className="text-primary underline font-bold " target="_blank" rel="noopener noreferrer">Get Directions</a>
+            <a href={data?.mapAddress} className="text-primary underline font-bold " target="_blank" rel="noopener noreferrer">Get Directions</a>
             <div className="grid mt-3">
 
                 <a className='font-bold' href={`tel:+${data?.mobile?.replaceAll(' ', '')}`}>Mobile: {data?.mobile}</a>
