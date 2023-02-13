a9ff2c35f7db5e9a8fea56709f143229e33fd785
ameenSpeakerbox <ameen466speakerbox@gmail.com>
2023-02-13 11:59:08 +0530

    modified:   src/pages/OurServices.js

--- src/pages/OurServices.js
+++ src/pages/OurServices.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import Layout from '../Components/Layout/Layout'
-import OurServices from './OurServices/OurServices'
+import React from "react";
+import Layout from "../Components/Layout/Layout";
+import OurServices from "./OurServices/OurServices";
 
 const ourservices = () => {
-    return (
-        <Layout>
-            <OurServices />
-        </Layout>
+  return (
+    <Layout>
+      <OurServices />
+    </Layout>
+  );
+};
 
-    )
+export function Head() {
+  return <title>Our Services | EFS Logistics KSA </title>;
 }
-export default ourservices
+export default ourservices;
